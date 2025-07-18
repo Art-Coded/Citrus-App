919c0385e6e62d4b322e0d11791e3b08e93b52a3
Art laptop <lyndonehemplo1@gmail.com>
2025-07-18 17:16:00 +0800

    dagger hilt to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,10 +89,8 @@ dependencies {
 
     implementation(libs.coil.compose)
 
-
-
-    implementation("com.google.dagger:hilt-android:2.51")
-    kapt("com.google.dagger:hilt-compiler:2.51")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation(libs.hilt.android)                       //implementation("com.google.dagger:hilt-android:2.51")
+    kapt(libs.hilt.compiler)                                //kapt("com.google.dagger:hilt-compiler:2.51")
+    implementation(libs.androidx.hilt.navigation.compose)   //implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
 }
